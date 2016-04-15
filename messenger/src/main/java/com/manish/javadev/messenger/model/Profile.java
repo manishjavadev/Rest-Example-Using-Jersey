1d82abf9a9904b33345d182714da9f21ce066a65
manishjavadev <msmanish95@gmail.com>
2016-04-16 01:13:15 +0530

    Progile Class Added

---
+++ messenger/src/main/java/com/manish/javadev/messenger/model/Profile.java
@@ -0,0 +1,72 @@
+package com.manish.javadev.messenger.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Profile {
+	private int id;
+	private String profileName;
+	private String firstName;
+	private String lastName;
+	private String author;
+
+	public Profile() {
+	}
+
+	
+	
+	public Profile(int id, String profileName, String firstName,
+			String lastName, String author) {
+		super();
+		this.id = id;
+		this.profileName = profileName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.author = author;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+
+}
--- messenger/src/main/java/com/manish/javadev/messenger/service/MessageService.java
+++ messenger/src/main/java/com/manish/javadev/messenger/service/MessageService.java
@@ -10,13 +10,30 @@ public class MessageService {
 
 	public List<Message> getAllMessage() {
 		List<Message> list = new ArrayList<Message>();
-		Message message1 = new Message(1, "Hello Manish", new Date(), "Akansha");
-		Message message2 = new Message(2, "Hello Ashis", new Date(), "Adarsh");
+		Message message1 = new Message(1, "Hello Manish", new Date(),
+				"Akanksha");
+		Message message2 = new Message(2, "Hello Ashish", new Date(), "Adarsh");
 		Message message3 = new Message(3, "Hello Manish", new Date(), "Divya");
 		list.add(message1);
 		list.add(message2);
 		list.add(message3);
 		return list;
+	}
+
+	public Message getMessageById(long messageId) {
+		return null;
+	}
+
+	public Message addMessage(Message message) {
+		return null;
+	}
+
+	public Message updateMessage(Message message) {
+		return null;
+	}
+
+	public Message removeMessage(Message message) {
+		return null;
 	}
 
 }
